fd12fd5319334971b87966fa4d5042d0401e4831
Mackenzie <mackbaird@outlook.com>
2024-04-21 22:44:59 +0100

    Displaying more info
    Now displays the searched city's humidity, wind speed, and weather description. Also shows matching weather description icon. And now also says the units after the temperature.

--- index.js
+++ index.js
@@ -30,11 +30,35 @@ let timeHTML = document.querySelector("#time");
 timeHTML.innerHTML = formatDate(currentTime);
 
 function displayTemperature(response) {
-  let temperatureElement = document.querySelector(".temperature");
+  let temperatureElement = document.querySelector(".displayTemp");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#searched-city");
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
+}
+
+function displayWindSpeed(response) {
+  let windSpeedElement = document.querySelector(".windSpeed");
+  let windSpeedMiles = Math.round(response.data.wind.speed);
+  windSpeedElement.innerHTML = `${windSpeedMiles} m/h`;
+}
+
+function displayHumidity(response) {
+  let humidityElement = document.querySelector(".humidityPercentage");
+  let cityHumidity = response.data.temperature.humidity;
+  humidityElement.innerHTML = `${cityHumidity}%`;
+}
+
+function displayWeatherCondition(response) {
+  let weatherConditionElement = document.querySelector(".weatherCondition");
+  let conditionDescription = response.data.condition.description;
+  weatherConditionElement.innerHTML = conditionDescription;
+}
+
+function displayWeatherIcon(response) {
+  let weatherIconElement = document.querySelector(".material-symbols-outlined");
+  let currentWeatherIcon = `<img src="${response.data.condition.icon_url}"></img>`;
+  weatherIconElement.innerHTML = currentWeatherIcon;
 }
 
 function search(event) {
@@ -44,6 +68,10 @@ function search(event) {
   let apiKey = "a1baf73doab5c6t69019f5ca71e4d0af";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayWindSpeed);
+  axios.get(apiUrl).then(displayHumidity);
+  axios.get(apiUrl).then(displayWeatherCondition);
+  axios.get(apiUrl).then(displayWeatherIcon);
 }
 
 let searchForm = document.querySelector("#search-form");
